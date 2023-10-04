65e2ce73f0bd3d2a9ee11d861e3d1aef6e4afa6a
Casper Ståhl <cstahl20@student.aau.dk>
2023-10-04 19:12:26 +0200

    Initialize BW.verbose in it's constructor

--- jajapy/base/BW.py
+++ jajapy/base/BW.py
@@ -14,6 +14,7 @@ class BW:
 	Class for the Baum-Welch algorithm.
 	"""
 	def __init__(self):
+		self.verbose = 2
 		try:
 			from ..with_stormpy import jajapyModeltoStormpy, stormpyModeltoJajapy
 			self.jajapyModeltoStormpy = jajapyModeltoStormpy
