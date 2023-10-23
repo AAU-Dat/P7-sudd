aed535998e2608504f915739981294dd141e530d
Casper Ståhl <cstahl20@student.aau.dk>
2023-10-23 15:34:54 +0200

    Refactor symbolic_to_numeric

--- jajapy/base/symbolic_to_numeric.c
+++ jajapy/base/symbolic_to_numeric.c
@@ -1,25 +1,25 @@
 #include "symbolic_to_numeric.h"
-// #define SET_BIT(array, pos)   (array[(pos) / 8] |= (1 << ((pos) % 8)))
-// #define CLEAR_BIT(array, pos) (array[(pos) / 8] &= ~(1 << ((pos) % 8)))
-// #define GET_BIT(array, pos)   ((array[(pos) / 8] & (1 << ((pos) % 8))) != 0)
+#include <stdio.h>
 
 CUDD_VALUE_TYPE** symbolic_to_numeric(DdNode* symbolic, int n_rows, int n_columns) {
-    unsigned int n_row_variables = (int) ceil(log2(n_rows));
-    unsigned int n_column_variables = (int) ceil(log2(n_columns));
+    int n_row_variables = (int) ceil(log2(n_rows));
+    int n_column_variables = (int) ceil(log2(n_columns));
     bool row_variables[n_row_variables];
     bool column_variables[n_column_variables];
+    bool bits[n_row_variables + n_column_variables];
     memset(row_variables, 0, sizeof(row_variables));
     memset(column_variables, 0, sizeof(column_variables));
     CUDD_VALUE_TYPE** result = create_2d_array(n_rows, n_columns);
 
     DdNode* root = symbolic;
     for (int i = 0; i < n_rows; i++) {
-        memset(column_variables, 0, sizeof(column_variables));
         for (int j = 0; j < n_columns; j++) {
-            result[i][j] = evaluate_matrix_BDD(root, row_variables, n_row_variables, column_variables, n_column_variables, 0);
+            interleave(row_variables, n_row_variables, column_variables, n_column_variables, bits);
+            result[i][j] = evaluate_matrix_BDD(root, bits, 0);
             increment_bit_array(column_variables, n_column_variables);
         }
         increment_bit_array(row_variables, n_row_variables);
+        memset(column_variables, 0, sizeof(column_variables));
     }
 
     return result;
@@ -33,16 +33,14 @@ CUDD_VALUE_TYPE** create_2d_array(int n_rows, int n_columns) {
     return array;
 }
 
-CUDD_VALUE_TYPE evaluate_matrix_BDD(DdNode* node, bool row_variables[], int n_row_variables, bool column_variables[], int n_column_variables, int index) {
-    bool bits[n_row_variables + n_column_variables];
-    interleave(row_variables, n_row_variables, column_variables, n_column_variables, bits);
+CUDD_VALUE_TYPE evaluate_matrix_BDD(DdNode* node, bool bits[], int index) {
     if (Cudd_IsConstant(node)) {
         return Cudd_V(node);
     }
     if (bits[index]) {
-        return evaluate_matrix_BDD(Cudd_T(node), row_variables, n_row_variables, column_variables, n_column_variables, index + 1);
+        return evaluate_matrix_BDD(Cudd_T(node), bits, index + 1);
     } else {
-        return evaluate_matrix_BDD(Cudd_E(node), row_variables, n_row_variables, column_variables, n_column_variables, index + 1);
+        return evaluate_matrix_BDD(Cudd_E(node), bits, index + 1);
     }
 }
 
@@ -73,53 +71,4 @@ void increment_bit_array(bool array[], int array_size) {
         }
     }
 }
-
-// void interleave( A[], unsigned char B[], unsigned char result[], int size) {
-//     int indexA, indexB, indexR;
-//     unsigned char maskA, maskB;
-//
-//     for (int i = 0; i < size * 8; i++) {
-//         indexA = i / 8;
-//         indexB = i / 8;
-//         indexR = i / 4;
-//
-//         maskA = 1 << (7 - (i % 8));
-//         maskB = 1 << (7 - (i % 8));
-//
-//         if (A[indexA] & maskA) {
-//             result[indexR] |= (1 << (7 - (2 * (i % 8))));
-//         }
-//
-//         if (B[indexB] & maskB) {
-//             result[indexR] |= (1 << (6 - (2 * (i % 8))));
-//         }
-//     }
-// }
-
-// CUDD_VALUE_TYPE evaluate_matrix_BDD(DdNode* node, unsigned char row_variables[], unsigned char column_variables[], size_t total_size) {
-//     unsigned char bits[total_size];
-//     interleave(row_variables, column_variables, bits, total_size);
-//     if (Cudd_IsConstant(node)) {
-//         return Cudd_V(node);
-//     }
-//     int index = Cudd_NodeReadIndex(node);
-//     if (GET_BIT(bits, index)) {
-//         return evaluate_matrix_BDD(Cudd_T(node), row_variables, column_variables, total_size);
-//     } else {
-//         return evaluate_matrix_BDD(Cudd_E(node), row_variables, column_variables, total_size);
-//     }
-// }
-
-// void increment_bit_array(unsigned char *array, size_t array_size) {
-//     int total_bits = array_size * 8;
-//
-//     for (int i = total_bits - 1; i >= 0; i--) {
-//         if (GET_BIT(array, i) == 0) {
-//             SET_BIT(array, i);
-//             return;
-//         } else {
-//             CLEAR_BIT(array, i);  // Set the bit to 0 and carry to the next bit
-//         }
-//     }
-// }
 
