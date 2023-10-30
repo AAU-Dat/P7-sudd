ca5d85930cb760f305a37ec17b2ef774a7cc3a39
Casper Ståhl <cstahl20@student.aau.dk>
2023-10-30 13:52:44 +0100

    Stuff

--- SUDD/sudd.py
+++ SUDD/sudd.py
@@ -3,38 +3,105 @@ from typing import Tuple, Any
 import numpy as np
 import os
 
-module = ctypes.CDLL("build/sudd.so")
+lib = ctypes.CDLL("build/sudd.so")
+
+lib.file_forwards.argtypes, lib.file_backwards.argtypes = [
+        ctypes.POINTER(ctypes.c_char_p),
+        ctypes.c_char_p,
+        ctypes.c_char_p,
+        ctypes.c_int,
+        ctypes.c_int
+    ]
+lib.file_forwards.restype, lib.file_backwards.restype \
+        = ctypes.POINTER(ctypes.POINTER(ctypes.c_double))
 
 
-def symbolic_compute_alphas_timed(
-    phi: np.ndarray[np.float64, Tuple[int, int]],
+def symbolic_forwards_timed(
+    phis: np.ndarray[np.float64, Tuple[int, int]],
     tau: np.ndarray[np.float64, Tuple[int, int]],
     pi: np.ndarray[np.float64, Tuple[int]],
-    n_states: int,
-    n_obs: int
 ) -> np.ndarray[np.float64, Tuple[int, int]]:
-    phi = numpy_to_file(phi, f"phi{os.getpid()}")
-    tau = numpy_to_file(tau, f"tau{os.getpid()}")
-    pi = numpy_to_file(pi, f"pi{os.getpid()}")
-    return module.file_forwards(phi, tau, pi)
+    return symbolic_fb_timed(
+        lib.file_forwards,
+        phis,
+        tau,
+        pi
+    )
 
 
-def symbolic_compute_betas_timed(
-    phi: np.ndarray[np.float64, Tuple[int, int]],
+def symbolic_backwards_timed(
+    phis: np.ndarray[np.float64, Tuple[int, int]],
     tau: np.ndarray[np.float64, Tuple[int, int]],
     pi: np.ndarray[np.float64, Tuple[int]],
-    n_states: int,
-    n_obs: int
 ) -> np.ndarray[np.float64, Tuple[int, int]]:
-    phi = numpy_to_file(phi, f"phi{os.getpid()}")
-    tau = numpy_to_file(tau, f"tau{os.getpid()}")
-    pi = numpy_to_file(pi, f"pi{os.getpid()}")
-    return module.file_backwards(phi, tau, pi)
+    return symbolic_fb_timed(
+        lib.file_backwards,
+        phis,
+        tau,
+        pi
+    )
+
+
+def symbolic_fb_timed(
+    fb: Any,
+    phis: np.ndarray[np.float64, Tuple[int, int]],
+    tau: np.ndarray[np.float64, Tuple[int, int]],
+    pi: np.ndarray[np.float64, Tuple[int]],
+) -> np.ndarray[np.float64, Tuple[int, int]]:
+
+    n_obs, n_states = phis.shape
+
+    # convert / make files
+    phi_file_names = [
+        f"phi_{i}_{os.getpid()}"
+        for i, phi in enumerate(phis)
+    ]
+    phis = [
+        numpy_to_file(phi, f"phi_{i}_{os.getpid()}")
+        for i, phi in enumerate(phis)
+    ]
+    # TODO len(phis) might be wrong
+    phis_p = (ctypes.c_char_p * len(phis))(*phis)
+
+    tau_file_name = f"tau_{os.getpid()}"
+    tau = numpy_to_file(tau, tau_file_name)
+
+    pi_file_name = f"pi_{os.getpid()}"
+    pi = numpy_to_file(pi, pi_file_name)
+
+    # TODO free ab
+    ab = fb(phis_p, tau, pi, n_states, n_obs - 1)
+
+    # convert ab to nparray
+    ab = c_array_to_numpy_array(ab, n_obs, n_states)
+
+    # cleanup
+    for phi_file_name in phi_file_names:
+        os.remove(phi_file_name)
+    os.remove(tau_file_name)
+    os.remove(pi_file_name)
+
+    return ab
+
+
+def c_array_to_numpy_array(c_array, rows, cols):
+    # Create an uninitialized array of given shape and type
+    np_array = np.empty((rows, cols), dtype=np.float64)
+    for i in range(rows):
+        for j in range(cols):
+            np_array[i, j] = c_array[i][j]
+    return np_array
 
 
 def numpy_to_file(
-    arr: np.ndarray[Any, np.dtype[Any]],
-    file: str
+    array: np.ndarray[Any, np.dtype[Any]],
+    filename: str
 ) -> ctypes.c_char_p:
-    # c_char_p = ctypes.c_char_p(python_string.encode('utf-8'))
-    NotImplemented
+    with open(filename, 'w') as f:
+        f.write(str(array.shape[0]) + ' ' + str(array.shape[1]) + '\n')
+        for i in range(array.shape[0]):
+            for j in range(array.shape[1]):
+                if array[i, j] != 0:
+                    f.write(f"{i} {j} {array[i, j]}\n")
+    f.close()
+    return ctypes.c_char_p(filename.encode('utf-8'))
