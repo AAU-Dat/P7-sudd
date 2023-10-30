a2e8188f6496bbefcf33441a4f9da54d8056b29b
Casper Ståhl <cstahl20@student.aau.dk>
2023-10-30 10:03:28 +0100

    Cleanup

--- SUDD/sudd.py
+++ SUDD/sudd.py
@@ -1,19 +1,3 @@
-from ctypes import *
-import ctypes
-import numpy as np
+from ctypes import CDLL
 
-# p = np.array([[1,2,3], [4, 5, 6], [7, 8, 9]], dtype=np.double)
-# p = p.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-# omega =np.array([[1,2,3], [4, 5, 6], [7, 8, 9]], dtype=np.double)
-# omega = omega.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-# pi = np.array([1,2,3], dtype=np.double)
-# pi = pi.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-
-# testmod = CDLL("/usr/local/lib/libcudd.so")
 module = CDLL("build/sudd.so")
-
-# forwards = module.forwards
-# forwards.restype = POINTER(c_double)
-# forwards.argtypes = [POINTER(c_double), POINTER(c_double), POINTER(c_double), c_int, c_int]
-#
-# forwards(p, omega, pi, 3, 2)
