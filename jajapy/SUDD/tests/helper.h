ce323d272c4fa6dcd360100db3b403ad01ab43fd
Anders Malta Jakobsen <amja23@student.aau.dk>
2023-11-08 20:29:15 +0100

    forwards_numeric() should work

--- jajapy/SUDD/tests/helper.h
+++ jajapy/SUDD/tests/helper.h
@@ -11,5 +11,6 @@ DdNode* matrix_2x2(DdManager* manager, double matrix[2][2], DdNode* row_var, DdN
 DdNode* matrix_3x3(DdManager* manager, double matrix[3][3], DdNode* row_vars[2], DdNode* col_vars[2]);
 DdNode* vector_2x1(DdManager* manager, double vector[2], DdNode* row_var);
 DdNode* vector_3x1(DdManager* manager, double vector[3], DdNode* row_vars[2]);
-
+double** forwards_numeric(double** omega, double** P, double* pi, int n_states, int n_obs);
+double** backwards_numeric(double** omega, double** P, double* pi, int n_states, int n_obs);
 #endif
