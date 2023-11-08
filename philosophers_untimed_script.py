0c531416b2901b2c5fe3e51a01b0c333aa941e2c
sebastian <saahol20@student.aau.dk>
2023-11-08 09:04:19 +0100

    add all the results, and some of the scripts run
    Former-commit-id: eaff2b4d054f0e827c72ae07435decffecaf8a8b

---
+++ philosophers_untimed_script.py
@@ -0,0 +1,16 @@
+import jajapy  as ja
+import os.path
+
+name = "results_philosophers_untimed.txt"
+file1 = open(name, "w")
+original_model = ja.loadPrism("examples/materials/philosophers1.sm")
+original_model.instantiate(["alpha", "betax", "zetax"], [0.3, 0.1, 0.2])
+for iter in range (1,11):
+	file1.write("run"+ str(iter) + "\n")
+	training_set = original_model.generateSet(100, 30, timed=False)
+	initial_hypothesis = ja.loadPrism("examples/materials/philosophers1.sm")
+	output_val = ja.BW().fit_nonInstantiatedParameters(training_set, initial_hypothesis, min_val=0.00025, max_val=0.0025, return_data=True)
+	file1.write(str(output_val))
+	file1.write("\n")
+	
+file1.close()
---
+++ tandem_script.py
@@ -0,0 +1,16 @@
+import jajapy  as ja
+import os.path
+
+name = "results_tadem_untimed.txt"
+file1 = open(name, "w")
+original_model = ja.loadPrism("examples/materials/tandem_3.sm")
+original_model.instantiate(["mu1a", "mu1b", "mu2", "kappa"], [0.2, 1.8, 2.0, 4.0])
+for iter in range (1,11):
+	file1.write("run"+ str(iter) + "\n")
+	training_set = original_model.generateSet(100, 30, timed=False)
+	initial_hypothesis = ja.loadPrism("examples/materials/tandem_3.sm")
+	output_val = ja.BW().fit_nonInstantiatedParameters(training_set, initial_hypothesis, min_val=0.00025, max_val=0.0025, return_data=True)
+	file1.write(str(output_val))
+	file1.write("\n")
+	
+file1.close()
